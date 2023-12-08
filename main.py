b531b4644a353adc02ddeaa21b3bbd12d354255b
ricardotran92 <quanghung.850621@gmail.com>
2023-12-08 08:19:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pickle as pkl
+import numpy as np
+
+class_list = {'0': 'Negative', '1': 'Positive', '2': 'Positive'}
+
+st.title('Sentiment analysis from Vietnamese students' feedback')
+
+image = Image.open('feedback.jpg')
+st.image(image)
+
+input_ec = open('ec_vsfc_pkl', 'rb')
+model 
+
 
