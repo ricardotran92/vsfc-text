dc107286c2478a14a6ce976ce7e9b6e65725015a
ricardotran92 <quanghung.850621@gmail.com>
2023-12-08 08:23:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ st.title('Sentiment analysis from Vietnamese students\' feedback')
 image = Image.open('feedback.jpg')
 st.image(image)
 
-input_ec = open('ec_vsfc_pkl', 'rb')
-model 
+input_ec = open('ec_vsfc.pkl', 'rb')
+encoder = pkl.load(input_ec)
+
+input_md = open('lrc_vsfc.pkl, 'rb')
+model = pkl.load(input_md)
+
+st.header('Write a feedback')
+txt = st.text_area('', '')
 
 
