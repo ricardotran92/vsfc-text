bd11080dff32da1f2eb53155da65911269fe7439
ricardotran92 <quanghung.850621@gmail.com>
2023-12-08 08:21:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class_list = {'0': 'Negative', '1': 'Positive', '2': 'Positive'}
 
-st.title('Sentiment analysis from Vietnamese students' feedback')
+st.title('Sentiment analysis from Vietnamese students\' feedback')
 
 image = Image.open('feedback.jpg')
 st.image(image)
